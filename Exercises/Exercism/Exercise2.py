7e95ecf07edb10b7e5ac6e451339c364c533c05d
Robergjo <roberg@hotmail.se>
2023-09-13 09:53:08 +0200

    Done with exercise

---
+++ Exercises/Exercism/Exercise2.py
@@ -0,0 +1,45 @@
+"""Functions used in preparing Guido's gorgeous lasagna.
+
+Learn about Guido, the creator of the Python language:
+https://en.wikipedia.org/wiki/Guido_van_Rossum
+
+This is a module docstring, used to describe the functionality
+of a module and its functions and/or classes.
+"""
+
+
+#TODO: define the 'EXPECTED_BAKE_TIME' constant.
+
+EXPECTED_BAKE_TIME = 40
+
+
+#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
+def bake_time_remaining(elapsed_bake_time) -> int:
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int - baking time already elapsed.
+    :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+
+    return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+
+#TODO: Define the 'preparation_time_in_minutes()' function below.
+# You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+
+def preparation_time_in_minutes(number_of_layers):
+    """Tell how long the preparation of the lasagna takes based on how many layers""" 
+    time = 2 * number_of_layers
+    return time
+
+
+#TODO: define the 'elapsed_time_in_minutes()' function below.
+# Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    """Calculates how long a lasagna has been prepared and cooked for"""
+    elapsed_time = preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+    return elapsed_time
